af3766b2628831b944968581061b78777dc22c62
DanyloSPTech <danylo.gomes@sptech.school>
2022-04-28 00:36:29 -0300

    tentando conectar o banco ao sistema de cadastro, ainda nao funciona

---
+++ projeto-banco/src/models/usuarioModel.js
@@ -0,0 +1,41 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nomeEmpresa, CNPJEmpresa, nome, email, senha, telefone) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao1 = `
+        INSERT INTO empresa (nomeEmpresa, CNPJ) VALUES ("${nomeEmpresa}", "${CNPJEmpresa}");        
+    `;
+    var instrucao2 = `
+        INSERT INTO usuario (nome, email, senha, telefone, fkEmpresa) VALUES ("${nome}", "${email}", "${senha}", "${telefone}", (select max(idEmpresa) from Empresa));
+    `
+    console.log("Executando a instrução SQL: \n" + instrucao1);
+    return database.executar(instrucao1 + " " + instrucao2);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
